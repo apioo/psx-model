1049eb2c5817e9cad02b3a364d88721c842837a9
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-04-08 13:03:30 +0200

    add external docs and info test

--- tests/Swagger/SwaggerTest.php
+++ tests/Swagger/SwaggerTest.php
@@ -20,6 +20,8 @@
 
 namespace PSX\Model\Tests\Swagger;
 
+use PSX\Model\Swagger\Contact;
+use PSX\Model\Swagger\ExternalDocs;
 use PSX\Model\Swagger\Header;
 use PSX\Model\Swagger\Headers;
 use PSX\Model\Swagger\Info;
@@ -30,8 +32,10 @@ use PSX\Model\Swagger\Path;
 use PSX\Model\Swagger\Response;
 use PSX\Model\Swagger\Responses;
 use PSX\Model\Swagger\Swagger;
+use PSX\Model\Swagger\Tag;
 use PSX\Schema\Parser;
 use PSX\Schema\Parser\Popo\Dumper;
+use PSX\Sql\Tests\TableManagerTest;
 
 /**
  * SwaggerTest
@@ -44,20 +48,34 @@ class SwaggerTest extends \PHPUnit_Framework_TestCase
 {
     public function testModel()
     {
+        $contact = new Contact();
+        $contact->setName('Swagger API Team');
+        $contact->setEmail('apiteam@swagger.io');
+        $contact->setUrl('http://swagger.io');
+
         $license = new License();
         $license->setName('MIT');
+        $license->setUrl('http://github.com/gruntjs/grunt/blob/master/LICENSE-MIT');
         
         $info = new Info();
         $info->setVersion('1.0.0');
         $info->setTitle('Swagger Petstore');
+        $info->setDescription('A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification');
+        $info->setTermsOfService('http://swagger.io/terms/');
+        $info->setContact($contact);
         $info->setLicense($license);
 
         $petSchema = json_decode('{ "required": [ "id", "name" ], "properties": { "id": { "type": "integer", "format": "int64" }, "name": { "type": "string" }, "tag": { "type": "string" } } }');
         $petsSchema = json_decode('{ "type": "array", "items": { "$ref": "#/definitions/Pet" } }');
         $errorSchema = json_decode('{ "required": [ "code", "message" ], "properties": { "code": { "type": "integer", "format": "int32" }, "message": { "type": "string" } } }');
 
+        $externalDocs = new ExternalDocs();
+        $externalDocs->setDescription('find more info here');
+        $externalDocs->setUrl('https://swagger.io/about');
+
         $swagger = new Swagger();
         $swagger->setInfo($info);
+        $swagger->setExternalDocs($externalDocs);
         $swagger->setHost('petstore.swagger.io');
         $swagger->setBasePath('/v1');
         $swagger->setSchemes(['http']);
@@ -175,157 +193,169 @@ class SwaggerTest extends \PHPUnit_Framework_TestCase
         $actual = json_encode($dumper->dump($swagger), JSON_PRETTY_PRINT); 
         $expect = <<<'JSON'
 {
-  "swagger": "2.0",
-  "info": {
-    "version": "1.0.0",
-    "title": "Swagger Petstore",
-    "license": {
-      "name": "MIT"
-    }
-  },
-  "host": "petstore.swagger.io",
-  "basePath": "/v1",
-  "schemes": [
-    "http"
-  ],
-  "consumes": [
-    "application/json"
-  ],
-  "produces": [
-    "application/json"
-  ],
-  "paths": {
-    "/pets": {
-      "get": {
-        "summary": "List all pets",
-        "operationId": "listPets",
-        "tags": [
-          "pets"
-        ],
-        "parameters": [
-          {
-            "name": "limit",
-            "in": "query",
-            "description": "How many items to return at one time (max 100)",
-            "required": false,
-            "type": "integer",
-            "format": "int32"
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "An paged array of pets",
-            "headers": {
-              "x-next": {
-                "type": "string",
-                "description": "A link to the next page of responses"
-              }
+    "swagger": "2.0",
+    "info": {
+        "title": "Swagger Petstore",
+        "version": "1.0.0",
+        "description": "A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification",
+        "termsOfService": "http:\/\/swagger.io\/terms\/",
+        "contact": {
+            "name": "Swagger API Team",
+            "url": "http:\/\/swagger.io",
+            "email": "apiteam@swagger.io"
+        },
+        "license": {
+            "name": "MIT",
+            "url": "http:\/\/github.com\/gruntjs\/grunt\/blob\/master\/LICENSE-MIT"
+        }
+    },
+    "host": "petstore.swagger.io",
+    "basePath": "\/v1",
+    "schemes": [
+        "http"
+    ],
+    "consumes": [
+        "application\/json"
+    ],
+    "produces": [
+        "application\/json"
+    ],
+    "paths": {
+        "\/pets": {
+            "get": {
+                "tags": [
+                    "pets"
+                ],
+                "summary": "List all pets",
+                "operationId": "listPets",
+                "parameters": [
+                    {
+                        "description": "How many items to return at one time (max 100)",
+                        "name": "limit",
+                        "in": "query",
+                        "required": false,
+                        "type": "integer",
+                        "format": "int32"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "An paged array of pets",
+                        "schema": {
+                            "$ref": "#\/definitions\/Pets"
+                        },
+                        "headers": {
+                            "x-next": {
+                                "description": "A link to the next page of responses",
+                                "type": "string"
+                            }
+                        }
+                    },
+                    "default": {
+                        "description": "unexpected error",
+                        "schema": {
+                            "$ref": "#\/definitions\/Error"
+                        }
+                    }
+                }
             },
-            "schema": {
-              "$ref": "#/definitions/Pets"
+            "post": {
+                "tags": [
+                    "pets"
+                ],
+                "summary": "Create a pet",
+                "operationId": "createPets",
+                "responses": {
+                    "201": {
+                        "description": "Null response"
+                    },
+                    "default": {
+                        "description": "unexpected error",
+                        "schema": {
+                            "$ref": "#\/definitions\/Error"
+                        }
+                    }
+                }
             }
-          },
-          "default": {
-            "description": "unexpected error",
-            "schema": {
-              "$ref": "#/definitions/Error"
+        },
+        "\/pets\/{petId}": {
+            "get": {
+                "tags": [
+                    "pets"
+                ],
+                "summary": "Info for a specific pet",
+                "operationId": "showPetById",
+                "parameters": [
+                    {
+                        "description": "The id of the pet to retrieve",
+                        "name": "petId",
+                        "in": "path",
+                        "required": true,
+                        "type": "string"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Expected response to a valid request",
+                        "schema": {
+                            "$ref": "#\/definitions\/Pets"
+                        }
+                    },
+                    "default": {
+                        "description": "unexpected error",
+                        "schema": {
+                            "$ref": "#\/definitions\/Error"
+                        }
+                    }
+                }
             }
-          }
         }
-      },
-      "post": {
-        "summary": "Create a pet",
-        "operationId": "createPets",
-        "tags": [
-          "pets"
-        ],
-        "responses": {
-          "201": {
-            "description": "Null response"
-          },
-          "default": {
-            "description": "unexpected error",
-            "schema": {
-              "$ref": "#/definitions/Error"
-            }
-          }
-        }
-      }
     },
-    "/pets/{petId}": {
-      "get": {
-        "summary": "Info for a specific pet",
-        "operationId": "showPetById",
-        "tags": [
-          "pets"
-        ],
-        "parameters": [
-          {
-            "name": "petId",
-            "in": "path",
-            "required": true,
-            "description": "The id of the pet to retrieve",
-            "type": "string"
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Expected response to a valid request",
-            "schema": {
-              "$ref": "#/definitions/Pets"
+    "definitions": {
+        "Pet": {
+            "required": [
+                "id",
+                "name"
+            ],
+            "properties": {
+                "id": {
+                    "type": "integer",
+                    "format": "int64"
+                },
+                "name": {
+                    "type": "string"
+                },
+                "tag": {
+                    "type": "string"
+                }
             }
-          },
-          "default": {
-            "description": "unexpected error",
-            "schema": {
-              "$ref": "#/definitions/Error"
+        },
+        "Pets": {
+            "type": "array",
+            "items": {
+                "$ref": "#\/definitions\/Pet"
             }
-          }
+        },
+        "Error": {
+            "required": [
+                "code",
+                "message"
+            ],
+            "properties": {
+                "code": {
+                    "type": "integer",
+                    "format": "int32"
+                },
+                "message": {
+                    "type": "string"
+                }
+            }
         }
-      }
+    },
+    "externalDocs": {
+        "description": "find more info here",
+        "url": "https:\/\/swagger.io\/about"
     }
-  },
-  "definitions": {
-    "Pet": {
-      "required": [
-        "id",
-        "name"
-      ],
-      "properties": {
-        "id": {
-          "type": "integer",
-          "format": "int64"
-        },
-        "name": {
-          "type": "string"
-        },
-        "tag": {
-          "type": "string"
-        }
-      }
-    },
-    "Pets": {
-      "type": "array",
-      "items": {
-        "$ref": "#/definitions/Pet"
-      }
-    },
-    "Error": {
-      "required": [
-        "code",
-        "message"
-      ],
-      "properties": {
-        "code": {
-          "type": "integer",
-          "format": "int32"
-        },
-        "message": {
-          "type": "string"
-        }
-      }
-    }
-  }
 }
 JSON;
 
